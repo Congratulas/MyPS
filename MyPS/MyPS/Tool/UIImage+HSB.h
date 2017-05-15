f9475aa7bc5b0ee1062f657ce447b6a6da284b5a
congratulas <congratulas@xiaoxia.local>
2017-05-15 09:57:02 +0800

    hsb

---
+++ MyPS/MyPS/Tool/UIImage+HSB.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+HSB.h
+//  MyPS
+//
+//  Created by congratulas on 2017/5/13.
+//  Copyright © 2017年 congratulas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIImage_HSB : NSObject
+
+@end
